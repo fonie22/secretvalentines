5a89602d3ecd3e295da5b3f4cde9dc37c23789a8
fonie22 <tnnatcharee@gmail.com>
2025-12-09 17:48:21 +0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ export function Layout({ children }: LayoutProps) {
   return (
   <div className="min-h-screen flex lg:items-center justify-center p-4 lg:overflow-hidden">
       <div className="container mx-auto max-w-5xl">
-        <div className="my-12 md:my-16 flex flex-col justify-around lg:flex-row gap-12 md:gap-16">
+        <div className="my-12 md:my-16 flex flex-col justify-around lg:flex-row gap-12 md:gap-16" style={{ width: '100%' }}>
           {children}
         </div>
       </div>
