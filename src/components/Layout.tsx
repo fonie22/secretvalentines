4068b2dd9abfd5c6e3c28e03e8e21fb34878fd48
fonie22 <tnnatcharee@gmail.com>
2025-12-09 15:22:59 +0700

    update style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,8 +7,13 @@ export type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex lg:items-center justify-center p-4 lg:overflow-hidden">
+  <div className="min-h-screen flex lg:items-center justify-center p-4 lg:overflow-hidden">
+      <div className="container mx-auto max-w-5xl">
+        <div className="my-12 md:my-16 flex flex-col justify-around lg:flex-row gap-12 md:gap-16">
           {children}
+        </div>
+      </div>
     </div>
+ 
   );
 }
