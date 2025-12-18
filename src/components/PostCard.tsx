9fc69d4bb18422e24cfb9c9136fc28c10c12a047
fonie22 <tnnatcharee@gmail.com>
2025-12-18 17:39:36 +0700

    Update PostCard.tsx

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -8,7 +8,7 @@ interface PostCardProps {
 
 export function PostCard({ children, className = "" }: PostCardProps) {
   return (
-    <div className={`shadow-md relative z-10 transform rotate-2 transition-transform duration-300 p-3 bg-postcard rounded-lg ${className}`}>
+    <div className={`shadow-[0_0_6px_6px_rgba(0,0,0,0.1)] relative z-10 transform rotate-2 transition-transform duration-300 p-3 bg-postcard rounded-lg ${className}`}>
       <div className="relative p-8 bg-[#FFE5EA]">
         <div className="absolute -top-6 -left-6 w-12 h-12 bg-[#5CC48A] rounded-full flex items-center justify-center transform -rotate-12">
           <img className={`w-full h-full`} src={christmasWreath} />
